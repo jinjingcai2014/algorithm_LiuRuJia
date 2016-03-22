134dc7c62e911a3b41aa469135b951344c7fdd12
jinjingcai2014 <690649591@qq.com>
2016-03-22 20:35:22 +0800

    新增加了两个例子

---
+++ Exercise5.2.3/main.cpp
@@ -0,0 +1,30 @@
+﻿
+///uva 10185
+///输入一个文本找出所有不同的单词，
+///按字典序从小到大输出，单词不区分大小写。
+
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <sstream>
+using namespace std;
+
+set<string>dict;
+int main()
+{
+	string s, buf;
+	while(cin >> s){
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isalpha(s[i]))s[i] = tolower(s[i]);
+            else s[i] = ' ';
+        }
+        stringstream ss(s);
+        while (ss >> buf)dict.insert(buf);
+	}
+	for (set<string>::iterator it = dict.begin(); it != dict.end(); ++it)
+		cout << *it << "\n";
+
+    return 0;
+}
---
+++ exercise5.2.4/main.cpp
@@ -0,0 +1,63 @@
+﻿/**uva 156 题意：不能通过字母重拍得到输入文中其他单词的单词
+*测试用例***
+ladder came tape soon leader acme RIDE lone Dreis peat
+ScAlE orb eye Rides dealer NotE derail LaCeS drIed
+noel dire Disk mace Rob dries
+#
+****************结果*************
+Disk
+NotE
+derail
+drIed
+eye
+ladder
+soon
+**/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+map<string, int>cnt;
+vector<string>words;
+
+//将单词进行标准化
+string repr(const string&s){
+    string ands = s;
+    for(int i = 0; i < ands.length(); i++)
+    {
+        ands[i] = tolower(ands[i]);
+    }
+    sort(ands.begin(), ands.end());
+    return ands;
+}
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int n = 0;
+    string s;
+    while(cin >> s){
+        if(s[0] == '#')break;
+        words.push_back(s);
+
+        //将单词进行标准化
+        string r = repr(s);
+        if(!cnt.count(r))cnt[r] = 0;
+        cnt[r]++;
+    }
+    vector<string>ans;
+    for(int i = 0; i < words.size(); i++)
+    {
+        if(cnt[repr(words[i])] == 1)ans.push_back(words[i]);
+    }
+    sort(ans.begin(), ans.end());
+    for(int i = 0; i < ans.size(); i++)
+        cout<<ans[i]<<"\n";
+    return 0;
+}
